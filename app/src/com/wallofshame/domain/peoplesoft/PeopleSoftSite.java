b05f40c3f3a0792a6fb79c297ce64dd3bdd74dcc
Hu Kai <iamkaihu@gmail.com>
2012-03-28 18:52:44 +0100

    move the function back to PeopleSoftSite, aggregate them together.

--- app/src/com/wallofshame/domain/peoplesoft/PeopleSoftSite.java
+++ app/src/com/wallofshame/domain/peoplesoft/PeopleSoftSite.java
@@ -1,16 +1,44 @@
 package com.wallofshame.domain.peoplesoft;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.wallofshame.domain.Credential;
+import com.wallofshame.domain.Employees;
+import com.wallofshame.domain.EmployeesParser;
+import com.wallofshame.repository.MissingTimeSheetRepositoryPeopleSoftImple;
+import org.apache.commons.lang.time.DateUtils;
+import org.apache.commons.lang.time.FastDateFormat;
+import org.apache.log4j.Logger;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Since: 3/19/12
  */
 public class PeopleSoftSite {
-
+    public static final Logger logger = Logger.getLogger(PeopleSoftSite.class);
     private final WebClient webClient;
 
     public PeopleSoftSite() {
         webClient = new WebClient();
+    }
+
+
+
+    public Employees fetch(String lastSunDay, String officeId) {
+        if (Credential.getInstance().isEmpty()) {
+            return new Employees();
+        }
+        try {
+            this.login(Credential.getInstance().username(), Credential.getInstance().password());
+            String cvsData = this.fetchCvsOfPeopleMissingTimesheet(lastSunDay, officeId);
+            return new EmployeesParser().parse(cvsData);
+        } catch (BadCredentialException badCredentialException) {
+            logger.info("Wrong password or username.Please check! People missing timesheet were not fetched.");
+        } finally {
+            this.cleanUp();
+        }
+        return new Employees();
     }
 
     public void login(String username, String password) throws BadCredentialException {
