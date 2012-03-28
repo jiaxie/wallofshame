02b90a46cc361082f6c4cec1a4fcf61e2967225f
Hu Kai <iamkaihu@gmail.com>
2012-03-28 19:08:28 +0100

    added the empty tests, so that people can learn how to test datetime related API, today should be banned in the code.

---
+++ app/test/com/wallofshame/service/UpdateWallOfShameServiceTest.java
@@ -0,0 +1,25 @@
+package com.wallofshame.service;
+
+import org.junit.Test;
+
+public class UpdateWallOfShameServiceTest {
+    @Test
+    public void should_return_last_sun_day_if_it_is_one_second_after_sunday() {
+
+    }
+
+    @Test
+    public void should_return_today_if_it_is_one_second_before_sunday_finished() {
+
+    }
+
+    @Test
+    public void should_return_last_sunday_if_today_is_tuesday() {
+
+    }
+
+    @Test
+    public void should_return_last_sunday_evn_today_is_anyday_cross_the_year() {
+
+    }
+}
